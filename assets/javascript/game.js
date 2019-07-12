d7bb9fb431ec224f841df5934aeb007b6113eac5
Jessie S <jessie.sholler@gmail.com>
2019-07-12 16:11:43 -0400

    Added functions to record wins, losses, guesses left, and current guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 // $(document).ready(function(){
 
 // Keyboard Input
-var computer_choices = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+var computer_choices = "aeiou";
 // console.log(computer_choices)
 
 // Creating variables to hold the number of wins and losses
 var user_wins = 0;
-var computer_wins = 0;
+var losses = 0;
 var remaining_guesses = 10; 
-var current_guesses = "";
+var current_guesses = [' ',];
 
  // Create variables that hold references to the places in the HTML where we want to display things.
 var user_guess_text = document.getElementById("user-guess");
@@ -16,7 +16,7 @@ var computer_choices_text = document.getElementById("computer-choice");
 var remaining_guesses_text = document.getElementById("remaining-guesses");
 var current_guesses_text = document.getElementById("current-guesses");
 var user_wins_text = document.getElementById("user-wins");
-var computer_wins_text = document.getElementById("computer-wins");
+var losses_text = document.getElementById("losses");
 
  // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
@@ -27,7 +27,26 @@ document.onkeyup = function(event) {
       var computer_guess = computer_choices[Math.floor(Math.random() * computer_choices.length)];
     //   console.log(computer_guess);
 
-      if (user_guess === computer_guess) {
-
+       if (user_guess === computer_guess) {
+            user_wins++;
+ 
         }
+        else if(remaining_guesses === 0) {
+            losses++;
+            remaining_guesses = 10;
+        } 
+        else {
+            remaining_guesses--;
+        }
+        
+        // Display the user and computer guesses, and wins/losses/ties.
+        user_guess_text.textContent = " " + user_guess;
+        computer_choices_text.textContent = " " + computer_guess;
+        current_guesses_text.innerHTML += " " + user_guess;
+        // console.log(current_guesses);
+        remaining_guesses_text.textContent = " " + remaining_guesses;
+        user_wins_text.textContent = " " + user_wins;
+        losses_text.textContent = " " + losses;
+
+
 };
